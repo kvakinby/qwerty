1ade98347ac8c97ede7f513a82ec6aebf54d44d5
Viachaslav <kvakinby@gmail.com>
2022-09-22 17:03:24 +0300

    my4commit

---
+++ compiled/dz_3.js
@@ -0,0 +1,5 @@
+var x = 20;
+var y = 58;
+var z = 42;
+var summ = x + y + z;
+console.log(summ);
---
+++ src/hw_3/task1.ts
@@ -0,0 +1,6 @@
+const x = 20;
+const y = 58;
+const z = 42;
+
+const summ = x + y + z;
+console.log(summ);
