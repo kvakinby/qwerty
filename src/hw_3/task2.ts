f9c8a7ccdeb6365904efba6beb24985fad24db78
Viachaslav <kvakinby@gmail.com>
2022-09-23 13:55:45 +0300

    my2commit

---
+++ src/hw_3/task2.ts
@@ -0,0 +1,8 @@
+const sekandmin = 60;
+const minandhour = 60;
+const hourandday = 24;
+const dayandyear = 365;
+const myage = 43;
+
+const myageinsecond: number = myage * dayandyear * hourandday * minandhour * sekandmin;
+console.log(`Myageinsecond - ${myageinsecond}`);
